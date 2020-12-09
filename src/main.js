fdd2d46c7d64b1ccd7e0776b0855aedcc055f86d
Majo Ávila Amezcua <mavila7@ucol.mx>
2020-12-08 18:03:23 -0600

    implemented sumatoraSerieUno()

--- src/main.js
+++ src/main.js
@@ -1,3 +1,22 @@
-export default class App {}
+export default class App {
+
+    sumatoriaSerieUno(numero) {
+        let suma = 1;
+
+        for(let i = 2; i <= numero; i = i + 1) {
+            suma = suma + 1 / i;
+        }
+        return suma;
+    }
+
+    
+
+
+}
 
 let app = new App();
+
+console.log("Probando sumatoriaSerieUno())");
+console.log(app.sumatoriaSerieUno(1));
+console.log(app.sumatoriaSerieUno(4));
+
